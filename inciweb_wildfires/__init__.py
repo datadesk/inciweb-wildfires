4066d85c2dd1daa6a9b6d7d7bdc6175213da2bf6
Ben Welsh <b@palewi.re>
2022-12-15 09:44:08 -0800

    Update __init__.py

--- inciweb_wildfires/__init__.py
+++ inciweb_wildfires/__init__.py
@@ -9,21 +9,34 @@ def get_incidents():
 
     Returns GeoJson FeatureCollection.
     """
+    # Get the URL
     url = "https://inciweb.nwcg.gov/feeds/maps/placemarks.kml"
     r = requests.get(url)
+
+    # Parse the KML
     soup = BeautifulSoup(r.content, "xml")
     folder_list = soup.find_all("Folder")
+
+    # Loop through all the placemarks
     feature_list = []
     for folder in folder_list:
+        # Pull out the properties
         d = dict(name=folder.find("name").text, url=folder.find("Placemark").a["href"])
+        
+        # Grab the coordinates
         coords = folder.find("Point").find("coordinates").text.split(",")
+        
+        # Reformat as GeoJSON
         p = Point(map(_safe_float, coords))
         f = Feature(geometry=p, properties=d)
         feature_list.append(f)
+    
+    # Pass it out
     return FeatureCollection(feature_list)
 
 
 def _safe_float(v):
+    """Handle the flawed coordinates published by Inciweb."""
     v = v.strip()
     # Fix the weird points that end with an extra "."
     v = ".".join(v.split(".")[:2])
